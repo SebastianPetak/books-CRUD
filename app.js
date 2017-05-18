2b33282187d8a8cde933a84a0bcbee6a5c897c8e
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-18 13:20:19 -0500

    Create more routes Create routes for retrieving and posting books to the database.

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Book = require('./Book.model');
-
+var port = process.env.PORT || 8080;
 var db = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mongoose-ex';
 
 mongoose.connect(db);
 
-var port = process.env.PORT || 8080;
+// Use bodyParser to parse json elements
+app.use(bodyParser.json())
+// Use urlencoded to give and recieve body elements through the url
+// Will be used with postman
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.get('/', function(req, res) {
 	res.send('We are at index')
@@ -38,6 +44,34 @@ app.get('/books/:id', function(req,res) {
 		} else {
 			console.log(book);
 			res.json(book);
+		}
+	})
+});
+// first and main way of creating
+app.post('/book', function(req, res) {
+	var newBook = new Book();
+
+	newBook.title = req.body.title;
+	newBook.author = req.body.author;
+	newBook.category = req.body.category;
+
+	newBook.save(function(err, book) {
+		if(err) {
+			res.send('error saving boko');
+		} else {
+			console.log(book);
+			res.send(book);
+		}
+	})
+});
+// second way of creating
+app.post('/book2', function(req,res) {
+	Book.create(req.body, function(err, book) {
+		if(err) {
+			res.send('error saving book');
+		} else {
+			console.log(book);
+			res.send(book);
 		}
 	})
 })
